6edb61e470d95d75a4ab35d809b9f10f74d4f119
Autumn <arartist@mit.edu>
2025-02-20 14:52:07 -0500

    Fixing links

--- global.js
+++ global.js
@@ -11,11 +11,11 @@ let currentLink = navLinks.find(a => a.host === location.host && a.pathname === 
 currentLink?.classList.add("current");
 
 let pages = [
-	{url: "", title: "Home"},
-	{url: "projects/", title: "Projects"},
-    {url: "contact/", title: "Contact"},
+	{url: "../", title: "Home"},
+	{url: "../projects/", title: "Projects"},
+    {url: "../contact/", title: "Contact"},
     {url: "https://github.com/autumnartist", title: "GitHub"},
-    {url: "resume/", title: "Resume"}
+    {url: "../resume/", title: "Resume"}
 ];
 
 let nav = document.createElement("nav");
@@ -26,9 +26,9 @@ const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 for (let p of pages) {
 	let url = p.url;
-    if (!ARE_WE_HOME && !url.startsWith("http")) {
-        url = "../" + url;
-    }    
+    // if (!ARE_WE_HOME && !url.startsWith("http")) {
+    //     url = "../" + url;
+    // }    
 	let title = p.title;
 	// Create link and add it to nav
     let a = document.createElement("a");
